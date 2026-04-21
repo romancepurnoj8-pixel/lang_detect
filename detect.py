f617f4866f63622a64945cd53664145fd1e2156b
ROMndlkcndA <happyminecrafter8@gmail.com>
2026-04-21 15:39:20 +0300

    add many symbol convert

--- detect.py
+++ detect.py
@@ -4,7 +4,12 @@ import time
 
 # Глобальные переменные для обмена данными
 char = ""
+ruSymbol = ""
+ru = ""
+source = "qwqwwqw"
+final_string = ""
 
+"""
 def getKey(key):
     global char
     
@@ -16,15 +21,26 @@ def getKey(key):
                 # Запуск прослушивания в фоновом потоке
 listener = keyboard.Listener(on_press=getKey)
 listener.start()
+"""
 
-def press(char):
-     # Поиск в словаре (возвращает сам символ, если ключа нет)
-    return ruDict.get(char, char)
+def ruSerch(char):
+    global ru
+    ru = ruDict.get(char, char)
+    return ru
 
+for c in source:
+        translated = ruSerch(c)
+        final_string += translated
+
+print(f"pressed: {source} in {final_string}")
+
+"""
 while True:
     if char != "":
-        ru = press(char)
+        #ru = ruSerch(char)
         print(f"Pressed: {char} in {ru}")                                                    
         # ОЧИЩАЕМ переменную, чтобы цикл дождался следующего нажатия
         char = ""
-    time.sleep(0.01)
+    time.sleep(0.010)
+"""
+
